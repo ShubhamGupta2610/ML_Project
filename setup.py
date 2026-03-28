b6bdd09f43b301724855c3b5a7f3b3e40ecfbc14
ShubhamGupta2610 <shubhamgupta262005@gmail.com>
2026-03-28 18:54:54 +0530

    logging and exception

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirement(file_path: str)->List[str]:
+    '''
+    This function will return the list of requirement'''
+    
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement=file_obj.readlines()
+        requirement=[req.replace("\n","") for req in requirement]
+
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+    return requirement
+setup(
+
+    name='ML_Project',
+    version='0.0.1',
+    author='Shubham',
+    author_email='shubhamgupta262005@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirement('requirement.txt')
+
+
+)
